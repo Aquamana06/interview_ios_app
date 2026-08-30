1b678ef02807422129d210d73d0a8fabd7c09fcd
Aquamana06 <namako-kabe@keio.jp>
2026-08-30 09:51:37 +0900

    Initial Commit

---
+++ ResilienceInterview/ResilienceInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResilienceInterviewApp.swift
+//  ResilienceInterview
+//
+//  Created by mana uchida on 2026/08/30.
+//
+
+import SwiftUI
+
+@main
+struct ResilienceInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
